c132646393b355ba2d5039d98cb0c77d19149b8b
Mateusz Janda <mateusz.janda@gmail.com>
2022-09-18 17:03:35 +0200

    Added main

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
+[bits 16]           ; generate code designed to run on a processor operating in 16-bit mode
 [org 0x7c00] ; MBR start address
 
+
+jmp main
 
     ; mov ah, 0x0e            ; BIOS command to move our cursor forward
     ; mov al, 'X'             ; Store the character to print in the al register
@@ -7,52 +10,57 @@
 
 ; mov ax, HELLO_MSG ; 1st variable
 
-; set video mode
-mov ah, 0x00
-mov al, 0x03
-int 0x10
+main:
+    ; set video mode
+    mov ah, 0x00
+    mov al, 0x03
+    int 0x10
 
 
-; set cursor position
-mov ah, 0x02
-mov dh, 1
-mov dl, 1
-int 0x10
+    ; set cursor position
+    mov ah, 0x02
+    mov dh, 1
+    mov dl, 1
+    int 0x10
 
-; set colors
-mov ah, 0x0b
-
-
+    ; set colors
+    mov ah, 0x0b
 
 
-mov bx, HAL_TEXT        ; pass argument
-call PRINT_TEXT    ; call the function
 
 
-jmp $
+    mov si, hal_text        ; pass argument
+    call print_text    ; call the function
 
 
-PRINT_TEXT:
+    jmp $
+
+
+print_text:
     pusha           ; push all registers to stack
-    mov ah, 0x0e    ; ;      BIOS command to move our cursor forward
-NEXT_CHAR:
-    mov al, [bx]        ; Store the character to print in the al register
-    xchg   cx, bx       ; Only BX can be used as an index register
-    mov bh, 0xc        ; color - light red
+    mov ah, 0x09    ; ;      Write Character and Attribute
+    mov     cx, 2
+    cld
+next_char:
+    lodsb
+    ; mov ah, 0x09
+    ; mov al, [bx]        ; Store the character to print in the al register
     cmp al, 0
-    je END_PRINT
+    je end_print
+    ; xchg   dx, bx       ; Only BX can be used as an index register
+    mov bl, 0x04        ; color - light red
     int 0x10        ;  BIOS interrupt - equivalent to print function
-    xchg   cx, bx       ; Only BX can be used as an index register
+    ; xchg   dx, bx       ; Only BX can be used as an index register
     inc bx
-    jmp NEXT_CHAR
-END_PRINT:
+    jmp next_char
+end_print:
     popa            ; pop all registers to stack
     ret
 
 
 
 
-HAL_TEXT:
+hal_text:
     db "I'm sorry Dave, I'm afraid I can't do that", 0
 
 times 510-($-$$) db 0   ;     Byte padding
